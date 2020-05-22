6f9b1438d5ad4d0f651455adff31525b08fe9f54
NTGustso <NTGustso@DESKTOP-0IA820K>
2020-05-21 17:27:42 -0700

    agregado variableas a Equipo

--- src/main/java/ar/edu/unju/edm/modelo/Equipo.java
+++ src/main/java/ar/edu/unju/edm/modelo/Equipo.java
@@ -1,5 +1,12 @@
 package ar.edu.unju.edm.modelo;
 
 public class Equipo {
+	private String nombre;
+	private int dni;
+	
+	
+	public Equipo() {
+			// TODO Auto-generated constructor stub
+		}
 
 }
--- src/main/java/ar/edu/unju/edm/service/AplicacionService.java
+++ src/main/java/ar/edu/unju/edm/service/AplicacionService.java
@@ -2,4 +2,5 @@ package ar.edu.unju.edm.service;
 
 public class AplicacionService {
 
+
 }
