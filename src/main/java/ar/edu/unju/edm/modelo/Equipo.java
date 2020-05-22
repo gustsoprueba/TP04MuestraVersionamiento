8e34061b33690ab78d6aa301be90c88913eece1c
NTGustso <NTGustso@DESKTOP-0IA820K>
2020-05-21 17:32:23 -0700

    agregados getter y setter

--- src/main/java/ar/edu/unju/edm/modelo/Equipo.java
+++ src/main/java/ar/edu/unju/edm/modelo/Equipo.java
@@ -9,4 +9,25 @@ public class Equipo {
 			// TODO Auto-generated constructor stub
 		}
 
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public int getDni() {
+		return dni;
+	}
+
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	
 }
